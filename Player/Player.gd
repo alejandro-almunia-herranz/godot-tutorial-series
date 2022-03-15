ad58d77b29f4c94b3e039fae8aa4be729d0dbce0
Alejandro Almunia Herranz <aalmunia@gmail.com>
2022-03-15 19:19:41 +0100

    Video6 Terminado

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,38 +15,11 @@ var velocity = Vector2.ZERO
 # Al usar onready var nos quitamos el método _ready()
 onready var animationPlayer = $AnimationPlayer
 
-# Esta función se llama cuando el nodo y sus hijos están listos para ser usados
-""" func _ready():
-	# Lo tenemos que meter aquí porque no está inicializado en el contructor
-	animationPlayer = $AnimationPlayer """
+onready var animationTree = $AnimationTree
 
-# Esta función se llama n veces por segundo (frames por segundo)
-# Este es el código que luego borra, lo dejo para referencia
-""" func _physics_process(delta):
-	# Si el botón predeterminado por la constante ui_right está pulsado	
-	if Input.is_action_pressed("ui_right"):
-		# Entonces ejecuta ese código
-		print("Has pulsado la tecla flecha derecha")
-		# Esto agrega 4 píxeles a la derecha a la posición en el eje horizontal del jugador
-		velocity.x += 1
-	# Si por contra el botón es izquierda
-	elif Input.is_action_pressed("ui_left"):
-		# Menos 4 píxeles al horizontal
-		velocity.x -= 1
-	# Si pulsamos tecla arriba
-	elif Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	# si pulsamos tecla abajo
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	# Y si no estamos pulsando ninguna, que se quede quieto
-	else:
-		velocity.x = 0git remote add origin https://github.com/alejandro-almunia-herranz/godot-tutorial-series.git
-		velocity.y = 0
-		
-	# Esto hace que el sprite se mueva Y COLISIONE con lo que sea que tenga colisión definida
-	move_and_collide(velocity) """
-	
+onready var animationState = animationTree.get("parameters/playback")
+
+
 # Esta es la función que realmente utiliza, a partir del minuto 27 del vídeo
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -61,23 +34,14 @@ func _physics_process(delta):
 	# Si tenemos algo en el vector de entrada
 	# Es decir, si han pulsado las teclas de movimiento
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animationPlayer.play("RunRight")
-		else:
-			animationPlayer.play("RunLeft")
-			
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		# Si no hay nada pulsado, deceleramos
-		animationPlayer.play("IdleRight")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	# Ahora usamos move_and_slide
 	# IMPORTANTE: NO USAMOS DELTA, ESTA FUNCIÓN YA LO HACE SOLO
 	velocity = move_and_slide(velocity)
-
-# Esta función se llama en cada frame. El argumento delta indica
-# el número de segundos que han pasado desde el último frame.
-# En el caso de 60 FPS, delta vale 0.0166666
-func _process(delta):
-	pass
